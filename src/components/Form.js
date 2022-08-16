df0f6cfd5b1d7844898576f08c43163aa7b843ed
Sabin Acharya <sabinacharya444@gmail.com>
2022-08-17 09:32:11 +1000

    State added on meme text

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,14 +3,31 @@ import memesData from "../memesData"
 
 export default function Form(){
     
-    const [memeImage , setMemeImage] = React.useState("")
+    const [memeImage , setMemeImage] = React.useState({
+        topText:"",
+        bottomText:"",
+        randomImage:""
+    })
+
+    function handleChange(event){
+        setMemeImage(prevMeme=>({
+            ...prevMeme,
+            [event.target.name] : event.target.value   
+        }))
+    
+    }
 
     let url
     function getMemeImage() {
         
         const memeArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memeArray.length)
-        setMemeImage(memeArray[randomNumber].url)
+        
+
+        setMemeImage(prevMeme => ({
+            ...prevMeme,
+            randomImage:memeArray[randomNumber].url
+        }))
         //url = memeArray[randomNumber].url  
         //console.log(url)
     }
@@ -19,14 +36,25 @@ export default function Form(){
            <input 
                 type="text"
                 placeholder="Top Text"
-                className="form--input"/>
+                className="form--input"
+                name="topText"
+                value={memeImage.topText}
+                onChange={handleChange}
+                />
            <input 
                 type="text"
                 placeholder="Bottom Text"
-                className="form--input"/>
+                className="form--input"
+                name="bottomText"
+                value={memeImage.bottomText}
+                onChange={handleChange}/>
 
            <button onClick={getMemeImage} className="form--button">Get a new meme image</button>
-           <img className="form--image" src={memeImage}></img>
+           <div className="meme">
+                <img src={memeImage.randomImage} className="form--image" />
+                <h2 className="meme--text top">{memeImage.topText}</h2>
+                <h2 className="meme--text bottom">{memeImage.bottomText}</h2>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react"
-import ReactDOM from "react-dom"
+//import ReactDOM from "react-dom/"
+import ReactDOM from "react-dom/client" //react 18
 import App from "./App"
 import "./style.css"
 
-ReactDOM.render(<App/>,document.getElementById("root"))
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>) //react 18
+
+//ReactDOM.render(<App/>,document.getElementById("root"))
